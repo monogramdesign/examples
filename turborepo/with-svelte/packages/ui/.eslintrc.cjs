a45a17b9fb8b1804c42c681a5d044c8192af3bfe
Liam Davis <liamdavis@tuta.io>
2021-12-28 15:20:26 -0700

    finish basic svelte example w/ shared component

---
+++ turborepo/with-svelte/packages/ui/.eslintrc.cjs
@@ -0,0 +1,2 @@
+// example of an eslint config being shared between projects
+module.exports = require('eslint/config')
---
+++ turborepo/with-svelte/packages/ui/Shared.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let projectName: string
+</script>
+
+<p>This is a shared component being used in the "{projectName}" project.</p>
+
+<style>
+	p {
+		@apply p-4 rounded-md shadow-md bg-blue-600 text-white font-semibold w-fit flex flex-col items-center space-y-4;
+	}
+</style>
