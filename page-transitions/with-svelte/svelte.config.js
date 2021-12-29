b607fb4ee4dc7446fdb2d82bde2cdceb8a906f44
Liam Davis <liamdavis@tuta.io>
2021-12-29 14:16:03 -0700

    test static build - svelte page transition

--- page-transitions/with-svelte/svelte.config.js
+++ page-transitions/with-svelte/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +13,6 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
-};
+}
 
-export default config;
+export default config
